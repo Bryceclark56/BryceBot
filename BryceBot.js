ff9444f8d8da85bc2bccde7bd56ada44dc94dfc4
Bryceclark56 <runescapebryce@yahoo.com>
2016-02-21 13:45:41 -0600

    Changes!

--- BryceBot.js
+++ BryceBot.js
@@ -1,10 +1,5 @@
-//var NetworkHandler = require("lib/NetworkHandler.js");
-//var mongoDB = require("mongodb");
-
-//var networks = NetworkHandler.loadNetworks();
-
 const Discord = require("./lib/discord.js");
-var uwot = new Discord(process.env.email, process.env.password, "test", "test");
+var uwot = new Discord(process.env.email, process.env.password);
 uwot.once("login", (data) => {
     console.log(data);
     uwot.openSocket();
@@ -12,4 +7,8 @@ uwot.once("login", (data) => {
 
 uwot.once("connected", () => {
     console.log("Hai");
+});
+
+uwot.on('data', (d) => {
+    console.log(d);
 });
